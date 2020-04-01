848be54d70698c243f3024ad9b616ccbe2233e4e
Samilly Nunes <samillynunes19@gmail.com>
2020-04-01 18:35:23 -0300

    First domain class tested

--- src/main/java/com/nunes/sam/resources/CategoriaResource.java
+++ src/main/java/com/nunes/sam/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package com.nunes.sam.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.nunes.sam.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias") //endpoint que vai ser a resposta p essa classe
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<Categoria>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 
 }
