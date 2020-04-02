0224a749e07c43f26d39f960aa52102a91b7f902
Samilly Nunes <samillynunes19@gmail.com>
2020-04-02 14:11:22 -0300

    Instantiation operation created

--- src/main/java/com/nunes/sam/CursomcApplication.java
+++ src/main/java/com/nunes/sam/CursomcApplication.java
@@ -1,13 +1,33 @@
 package com.nunes.sam;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.nunes.sam.domain.Categoria;
+import com.nunes.sam.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository repo;;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		//o arrays.aslist() cria a lista automaticamente
+		repo.saveAll(Arrays.asList(cat1,cat2));
+		
 	}
 
 }
