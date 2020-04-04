280a3523766a8ef8207928375e7d9c05b038dd2f
Samilly Nunes <samillynunes19@gmail.com>
2020-04-03 21:27:03 -0300

    Endpoint /clientes/id available

---
+++ src/main/java/com/nunes/sam/services/ClienteService.java
@@ -0,0 +1,27 @@
+package com.nunes.sam.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nunes.sam.domain.Categoria;
+import com.nunes.sam.domain.Cliente;
+import com.nunes.sam.repositories.ClienteRepository;
+import com.nunes.sam.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		//Busca no repositorio pelo id. O optional eh  para encapsular a questao de ser um obj instanciado ou nao. Feito para eliminar o problema do nulo.
+		Optional<Cliente> obj = repo.findById(id); 
+		return obj.orElseThrow(()-> new ObjectNotFoundException( //esta levantando uma excecao personalizada caso n encontre
+				"Objeto não encontrado! Id:"+id+", Tipo: "+Cliente.class.getName())); //Nesse caso, para retornar nulo se nao tiver encontrado, coloca o orelse(null) 
+				
+	}
+
+}
