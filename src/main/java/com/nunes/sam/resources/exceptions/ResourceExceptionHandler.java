16236a8a3c138fba345bb0cf3c9f8a917a5ecf4d
Samilly Nunes <samillynunes19@gmail.com>
2020-04-08 14:47:02 -0300

    Customizing the return of validation

--- src/main/java/com/nunes/sam/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/nunes/sam/resources/exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -26,4 +28,17 @@ public class ResourceExceptionHandler { //manipulador de excecoes dos controlado
 		
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
+	
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class) //pra dizer que eh um tratador de excecoes o tipo objectNotFound
+	public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request){
+		ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(),"Erro de validação",System.currentTimeMillis()); //bad request eh uma requisicao proibida?
+		
+		for(FieldError x : e.getBindingResult().getFieldErrors()) { //esta percorrendo o json que tem nas excecoes do tipo arumento nao valido e pegando somente o nome e a mensagem do erro
+			err.addError(x.getField(), x.getDefaultMessage());
+		}
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+	
 }
