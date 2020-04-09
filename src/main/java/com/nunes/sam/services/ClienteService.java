4f0f82ac53d1dfaf69a99f4bacf7d9fe7b2382ea
Samilly Nunes <samillynunes19@gmail.com>
2020-04-09 20:49:37 -0300

    Adjusting the deletion of the customer

--- src/main/java/com/nunes/sam/services/ClienteService.java
+++ src/main/java/com/nunes/sam/services/ClienteService.java
@@ -59,7 +59,7 @@ public class ClienteService {
 		try {
 			repo.deleteById(id);
 		} catch(DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possível deletar excluir porquê há entidades relacionadas");
+			throw new DataIntegrityException("Não é possível deletar porquê há pedidos relacionados");
 		}
 		
 	}
