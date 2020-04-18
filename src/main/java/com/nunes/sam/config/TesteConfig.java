5bb9aae187bbf8c75ffb4e9d9db8a3f44030f851
Samilly Nunes <samillynunes19@gmail.com>
2020-04-18 17:09:38 -0300

    Creating test user

---
+++ src/main/java/com/nunes/sam/config/TesteConfig.java
@@ -0,0 +1,27 @@
+package com.nunes.sam.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.nunes.sam.services.DBService;
+
+@Configuration
+@Profile("teste") //pra dizer que todos os beans daqui so serao ativados quando o perfil de teste estiver ativado
+public class TesteConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		
+		dbService.instantiateTestDatabase();
+		return true; //so pq eh obrigado
+	}
+
+}
